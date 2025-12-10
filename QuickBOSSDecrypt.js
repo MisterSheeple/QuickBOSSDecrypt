77e4ef8bbd33e665d36609e96d9b4b470c04f84d
MisterSheeple <27652712+MisterSheeple@users.noreply.github.com>
2025-12-10 01:34:23 +0000

    Add support for embedding key in the js
    This saves the user from having to enter the key for every command, but releases will not have this feature enabled.

--- QuickBOSSDecrypt.js
+++ QuickBOSSDecrypt.js
@@ -1,29 +1,45 @@
 const fs = require('node:fs');
-const boss = require('@pretendonetwork/boss-crypto')
+const boss = require('@pretendonetwork/boss-crypto');
+let help;
+let filename;
+let BOSS_AES_KEY;
 
-const platform = "js"
+const platform = "js";
 
-let help
-let filename
-let BOSS_AES_KEY
+// NOTE: You can uncomment the following line and fill in the BOSS AES key to avoid having to enter it manually for every command.
+// BOSS_AES_KEY = "boss_aes_key_goes_here"
 
 if (platform == "win64") {
-    help = `Usage: QuickBOSSDecrypt.exe <file> <BOSS AES key>`;
+    if (BOSS_AES_KEY == null) {
+        help = `Usage: QuickBOSSDecrypt.exe <file> <BOSS AES key>`;
+    } else {
+        help = `Usage: QuickBOSSDecrypt.exe <file>`;
+    };
 } else if (platform == "unixlike") {
-    help = `Usage: ./QuickBOSSDecrypt <file> <BOSS AES key>`;
+    if (BOSS_AES_KEY == null) {
+        help = `Usage: ./QuickBOSSDecrypt <file> <BOSS AES key>`;
+    } else {
+        help = `Usage: ./QuickBOSSDecrypt <file>`;
+    };
 } else if (platform == "js") {
-    help = `Usage: node QuickBOSSDecrypt.js <file> <BOSS AES key>`;
+    if (BOSS_AES_KEY == null) {
+        help = `Usage: node QuickBOSSDecrypt.js <file> <BOSS AES key>`;
+    } else {
+        help = `Usage: node QuickBOSSDecrypt.js <file>`;
+    };
 };
 
 process.argv.forEach(function (val, index, array) {
-    if (array.length != 4 || val == "--help" || val == "-h") {
+    if (array.length < 3 || array.length > 4 || array.length == 3 && BOSS_AES_KEY == null || val == "--help" || val == "-h") {
         console.log(help);
         process.exit(0);
     } else if (index == 2) {
         filename = val;
     } else if (index == 3) {
-        BOSS_AES_KEY = val;
-    }
+        if (BOSS_AES_KEY == null) {
+            BOSS_AES_KEY = val;
+        };
+    };
 });
 
 if (!fs.existsSync(filename)) {
@@ -34,4 +50,4 @@ if (!fs.existsSync(filename)) {
 const { payload_contents } = boss.decrypt3DS(filename, BOSS_AES_KEY);
 
 fs.writeFileSync(filename + ".dec", payload_contents[0].content);
-console.log("Successfully decrypted file.")
+console.log("Successfully decrypted file.");
